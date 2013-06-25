816189b71a3308e0307b50945bebf96be52a62de
socjopata <socjopata@gmail.com>
2013-06-25 12:30:54 +0200

    fixing uninitialized constant ActiveAdmin

--- lib/active_admin_importable.rb
+++ lib/active_admin_importable.rb
@@ -1,6 +1,7 @@
 require "active_admin_importable/version"
 require 'active_admin_importable/engine'
 require 'active_admin_importable/dsl'
+require 'activeadmin'
 
 ::ActiveAdmin::DSL.send(:include, ActiveAdminImportable::DSL)
 
