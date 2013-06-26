96bfb2ca495b970d89c8a11e4d581878ed7bd49e
socjopata <socjopata@gmail.com>
2013-06-26 17:00:54 +0200

    error flash message

--- lib/active_admin_importable/dsl.rb
+++ lib/active_admin_importable/dsl.rb
@@ -19,7 +19,7 @@ module ActiveAdminImportable
         unless  @something_went_wrong
           redirect_to :action => :index, :notice => "#{active_admin_config.resource_name.to_s} imported successfully!"
         else
-
+          redirect_to :action => :index, :error => "Something went wrong!"
         end
       end
     end
